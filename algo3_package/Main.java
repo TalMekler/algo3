781daeb9df68cac2347020ce5cd5b4b4e3d8ddec
Tal Mekler <tal.mekler11@gmail.com>
2022-11-28 14:39:44 +0200

    add more tests to prim alg

--- algo3_package/Main.java
+++ algo3_package/Main.java
@@ -7,14 +7,20 @@ public class Main {
     public static void main(String argv[]) {
 
         Graph graph = new Graph(new ArrayList<Vertex>(), new ArrayList<Edge>());
+        Graph graphPrim;
+
         graph.addEdge(new Edge(new Vertex("A"), new Vertex("B"), 3));
         graph.addEdge(new Edge(new Vertex("A"), new Vertex("C"), 2));
         graph.addEdge(new Edge(new Vertex("D"), new Vertex("A"), 4));
         graph.addEdge(new Edge(new Vertex("C"), new Vertex("D"), 2));
+        graph.addEdge(new Edge(new Vertex("C"), new Vertex("E"), 8));
+        graph.addEdge(new Edge(new Vertex("D"), new Vertex("E"), 5));
         graph.print();
-        System.out.println("--- Using prim alg ---");
-        graph.prim().print();
 
-        System.out.println("Done!");
+        System.out.println("\n--- Using prim alg ---");
+        graphPrim = graph.prim();
+        graphPrim.print();
+
+        System.out.println("\nDone!");
     }
 }
