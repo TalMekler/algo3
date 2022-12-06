b5af2fea54601f6a92163e2d0d4de8596e4e8353
Tal Mekler <tal.mekler11@gmail.com>
2022-12-06 15:22:04 +0200

    add members to vertex

--- algo3_package/Vertex.java
+++ algo3_package/Vertex.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 public class Vertex {
     private final String key;
+    private ArrayList<Vertex> neighbors;
+    private Vertex pi;
 
     public Vertex (String key) {
         this.key = key;
+        this.neighbors = new ArrayList<>();
+        this.pi = null;
     }
 
     public String getKey() {
